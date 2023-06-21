8517f3cf997aa74a8073e20526bb3a1d81d49ac3
Beppe <giuseppe.monorchio94@hotmail.it>
2023-06-21 20:53:41 +0200

    add function to sort and remove duplicate elements from array

--- BST.js
+++ BST.js
@@ -11,5 +11,20 @@ class Tree{
         this.root = this.buildTree(array);
     }
 
-    
+    sortAndRemove(array) {
+        //spread array elements in a new set
+        //then compare each other to sort them
+        const sorted = [...new Set(array)].sort((a,b) => a - b);
+
+        return sorted;
+    }
+
+    buildTree() {}
 }
+
+//testing method
+const arr = [3,4,2,5,7,2,1234,24,435,52,34,5,665,7]
+
+let tree = new Tree(3,4,2,5,7,2,1234,24,435,52,34,5,665,7);
+
+console.log(tree.sortAndRemove(arr))
